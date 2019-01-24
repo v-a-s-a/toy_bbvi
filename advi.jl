60724dc2f9afd96c8ff5fbf7a619912e7f3e5e75
Vassily Trubetskoy <vasa@Vassilys-MacBook-Pro.local>
2019-01-24 14:28:28 +0100

    Optimization with ADAM finally working

--- advi.jl
+++ advi.jl
@@ -1,13 +1,10 @@
-
 using Distributions
-<<<<<<< HEAD
-using Flux.Tracker: gradient, param, Params
-using Flux.Optimise: Descent, ADAM, update!
-=======
 using Flux
->>>>>>> 2855f3f32e7670ecf4eb22e5448be1449fb24d12
 using Plots
 using LinearAlgebra
+
+D = 2  # dimensions of approximate posterior
+num_samples = 100
 
 """
 A 2D non-gaussian log-density.
@@ -20,6 +17,49 @@ function log_density(params)
     d2_density = logpdf(d2, mu)
     return d1_density + d2_density
 end
+
+"""
+Entropy of the Gaussian distribution.
+"""
+function gaussian_entropy(log_std)
+    H = 0.5 * D * (1.0 + log(2 * pi)) + sum(log_std)
+    return H
+end
+
+"""
+Variational approximation to the non-gaussian density
+"""
+function variational_objective(parameters; D=2)
+    mu, log_std = parameters
+    samples = rand(Normal(), num_samples, D) .* exp.(log_std) .+ mu
+    log_px = mapslices(log_density, samples; dims=2) # eval log(target) for all samples of params (i.e. cols)
+    # println("entropy: $(gaussian_entropy(log_std))")
+    # println("mean: $(mean(log_px))")
+    elbo = gaussian_entropy(log_std) + mean(log_px)
+    return -elbo
+end
+
+mu = Flux.Tracker.param(reshape([-1, -1], 1, :))
+sigma = Flux.Tracker.param(reshape([-5, -5], 1, :))
+
+parameters = Flux.Tracker.Params([mu, sigma])
+elbo_gradient = Flux.Tracker.gradient(() -> variational_objective(parameters), parameters)
+
+elbo = [variational_objective(parameters)]
+steps = 200
+
+opt = ADAM(0.1)
+for i in 1:steps
+    println(i)
+    elbo_gradient = Flux.Tracker.gradient(() -> variational_objective(parameters), parameters)
+    for p in (mu, sigma)
+        Δ =  Flux.Optimise.update!(opt, p, Flux.data(elbo_gradient[p]))
+        Flux.Tracker.update!(p, -Δ)
+        push!(elbo, variational_objective(parameters))
+    end
+end
+
+## Plotting
 
 x = -2:0.1:2
 y = -4:0.1:2
@@ -34,69 +74,14 @@ for i in 1:size(X)[1]
     end
 end
 
-#plot(contour(x, y, Z))
-
-D = 2  # dimensions of approximate posterior
-num_samples = 100
-"""
-Entropy of the Gaussian distribution.
-"""
-function gaussian_entropy(log_std)
-    H = 0.5 * D * (1.0 + log(2 * pi)) + sum(log_std)
-    return H
-end
-
-function variational_objective(parameters; D=2)
-    mu, log_std = parameters
-    samples = rand(Normal(), num_samples, D) .* sqrt.(log_std) .+ mu
-    log_px = mapslices(log_density, samples; dims=2) # eval log(target) for all samples of params (i.e. cols)
-    elbo = gaussian_entropy(log_std) + mean(log_px)
-    return -elbo
-end
-
-mu = Flux.Tracker.param(reshape([-1, -1], 1, :))
-sigma = Flux.Tracker.param(reshape([1, 1], 1, :))
-
-<<<<<<< HEAD
-elbo_gradient = gradient(() -> variational_objective(mu, sigma), Params([mu, sigma]))
-
-opt = Descent(0.001)
-for p in (mu, sigma)
-    update!(opt, p, elbo_gradient[p])
-end
-=======
-parameters = Flux.Tracker.Params([mu, sigma])
-elbo_gradient = Flux.Tracker.gradient(() -> variational_objective(parameters), parameters)
->>>>>>> 2855f3f32e7670ecf4eb22e5448be1449fb24d12
-
-elbo = [variational_objective(parameters)]
-steps = 1000
-
-<<<<<<< HEAD
-η = 0.001
-opt = Descent(0.001)
-for i in 1:steps
-    println(i)
-    for p in (mu, sigma)
-        Flux.Optimise.update!(opt, p, elbo_gradient[p].data)
-=======
-opt = ADAM(0.01)
-for i in 1:steps
-    println(i)
-    for p in (mu, sigma)
-        Flux.Tracker.update!(p, Flux.Optimise.update!(opt, p, Flux.data(elbo_gradient[p])))
-        push!(elbo, variational_objective(parameters))
->>>>>>> 2855f3f32e7670ecf4eb22e5448be1449fb24d12
-    end
-end
 
 q = MultivariateNormal(mu[1,:].data, Diagonal(exp.(2*sigma[1,:].data)))
 Z_q = Array{Float64}(undef, size(X))
 for i in 1:size(X)[1]
     for j in 1:size(X)[2]
-        Z[i, j] = pdf(q, [X[i, j], Y[i, j]])
+        Z_q[i, j] = pdf(q, [X[i, j], Y[i, j]])
     end
 end
 
-plot(contour(x, y, Z_q))
-
+contour(x, y, Z)
+contour!(x, y, Z_q)
