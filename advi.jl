2115ecfaa6b035c08fe93d98a802c588a228849b
vasya <trubetskoy.vasa@gmail.com>
2019-01-24 10:31:56 +0100

    Tried merging paramters to no avail.

--- advi.jl
+++ advi.jl
@@ -1,7 +1,6 @@
 
 using Distributions
-using Flux.Tracker: gradient, param, Params
-using Flux.Optimise: Descent, ADAM, update!
+using Flux
 using Plots
 using LinearAlgebra
 
@@ -26,7 +25,7 @@ Y = repeat(y, 1, length(x))
 Z = Array{Float64}(undef, size(X))
 for i in 1:size(X)[1]
     for j in 1:size(X)[2]
-        Z[i, j] = exp(log_density([X[i,j], Y[i, j]]))
+        Z[i, j] = exp(log_density([X[i, j], Y[i, j]]))
     end
 end
 
@@ -42,42 +41,46 @@ function gaussian_entropy(log_std)
     return H
 end
 
-function variational_objective(mu, log_std; D=2)
-    samples = rand(Normal(), num_samples, D) .* sqrt.(log_std) .+ mu
+function variational_objective(parameters; D=2)
+    mu = reshape(parameters[1:D], 1, :)
+    log_std = reshape(parameters[D+1:end], 1, :)
+    samples = rand(Normal(), num_samples, D) .* exp.(log_std) .+ mu
     log_px = mapslices(log_density, samples; dims=2) # eval log(target) for all samples of params (i.e. cols)
     elbo = gaussian_entropy(log_std) + mean(log_px)
     return -elbo
 end
 
-mu = param(reshape([-1, -1], 1, :))
-sigma = param(reshape([5, 5], 1, :))
 
-elbo_gradient = gradient(() -> variational_objective(mu, sigma), Params([mu, sigma]))
+mu_init = [-1, -1]
+sigma_init = [-5, -5]
 
-opt = Descent(0.001)
-for p in (mu, sigma)
-    update!(opt, p, elbo_gradient[p])
-end
+parameters = Flux.Tracker.param(cat(mu_init, sigma_init, dims=1))
+elbo_gradient = Flux.Tracker.gradient(variational_objective, parameters)
 
-elbo = [variational_objective(mu, sigma)]
-steps = 100
+steps = 2000
+elbo = Array{Float64}(undef, steps)
 
-η = 0.001
-opt = Descent(0.001)
+mu_trajectory = Array{Float64}(undef, steps, D)
+sigma_trajectory = Array{Float64}(undef, steps, D)
+
+opt = ADAM(0.1)
 for i in 1:steps
     println(i)
-    for p in (mu, sigma)
-        Flux.Optimise.update!(opt, p, elbo_gradient[p].data)
-    end
+    Δ = Flux.Optimise.update!(opt, parameters, Flux.data(elbo_gradient[1]))
+    Flux.Tracker.update!(parameters, Δ)
+    elbo[i] = variational_objective(parameters.data)
+    mu_trajectory[i, :] = parameters[1:D].data
+    sigma_trajectory[i, :] = parameters[D+1:end].data
 end
 
 q = MultivariateNormal(mu[1,:].data, Diagonal(exp.(2*sigma[1,:].data)))
 Z_q = Array{Float64}(undef, size(X))
 for i in 1:size(X)[1]
     for j in 1:size(X)[2]
-        Z[i, j] = pdf(q, [X[i,j], Y[i, j]])
+        Z_q[i, j] = pdf(q, [X[i, j], Y[i, j]])
     end
 end
 
-plot(contour(x, y, Z_q))
-
+pyplot()
+contour(x, y, Z)
+contour!(x, y, Z_q)
